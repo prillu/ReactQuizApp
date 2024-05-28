ceb5b98e540156c47d9f8b82f4e57b09e6334dc2
prillu <prilnaunnimol@gmail.com>
2024-05-28 20:36:31 +0530

    update

---
+++ src/Data/QuizData.js
@@ -0,0 +1,31 @@
+export const QuizData =[
+    {
+        question : "JSX stands for...",
+        options:[ "JavaScript XML", "Java Syntax Extension", "JavaScript Syntax", "None of the Above"],
+        
+        answer: 1,
+    },
+    {
+        question : " What is the purpose of keys in React lists?",
+        options:["To enhance performance","To uniquely identify list items","To style list items","To handle events on list items"],
+        answer: 2,
+    },
+    {
+        question : "Identify the smallest building block of React.JS",
+        options:["Component","Props","State","Event"],
+        answer: 1,
+    },
+    {   
+        question: "What is ReactJS mainly used for building?",
+       options :["Database","Connectivity","User Interface","None of the Above"],
+        answer: 3,
+    },
+    {
+        question : "Which of the following are two ways to handle data in react?",
+        options:["State & Props","State & Redux","State & Hooks","State & Context"],
+        answer: 1,
+    }
+
+]
+
+// ];
